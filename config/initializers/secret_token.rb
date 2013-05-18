6c5c1ff9203d659b58f395d9dca7fc69e1403d09
Eric <eric@DEV1.(none)>
2013-05-18 11:20:43 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babynamer::Application.config.secret_token = '21c27290cff746ec496eaa824128b9fac5440e6867e8486736e574f1b15cd252bb54b2ebf1a06d6051dc7fa737327baf61fb3d54327e0e345c7f0ba6a75fed19'
